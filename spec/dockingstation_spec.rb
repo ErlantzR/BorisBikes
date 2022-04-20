17582221f70467a4149cd80fe8adf0c16dc7823c
Erlantz <erlantzram@gmail.com>
2022-04-20 16:41:49 +0100

    pass working? test

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,4 +4,9 @@ describe DockingStation do
 
   it {is_expected.to respond_to :release_bike}
 
+  it "Get a bike and expect method 'working?' to work" do
+    bike = subject.release_bike
+    expect(bike.working?).to eq(true)
+  end
+
 end
